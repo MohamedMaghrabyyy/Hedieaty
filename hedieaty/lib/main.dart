ecda80c335768a11c6de3e205a34215a58bdf7bb
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2024-10-23 18:44:24 +0300

    Font added and created a loading screen

--- hedieaty/lib/main.dart
+++ hedieaty/lib/main.dart
@@ -5,23 +5,71 @@ void main() {
   runApp(MaterialApp(
     initialRoute: '/loading',
     routes: {
-      '/loading': (context) => LoadingScreen(),
+      '/loading': (context) => const LoadingScreen(),
       '/login': (context) => LoginPage(),
       '/home': (context) => HomeScreen(),
     },
   ));
 }
 
-class LoadingScreen extends StatelessWidget {
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Navigate to HomeScreen after 3 seconds
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 58, 2, 80),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Hedieaty',
+                  style: TextStyle(
+                    fontSize: 36.0,
+                    color: Color(0xFFF8F8FF),
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "gvibes",
+                  ),
+                ),
+                SizedBox(width: 10),
+                Icon(
+                  Icons.card_giftcard,
+                  color: Color.fromARGB(255, 248, 220, 9),
+                  size: 30.0,
+                ),
+              ],
+            ),
+            SizedBox(height: 50),
             SpinKitSpinningLines(
-              color: Colors.black,
+              color: Color(0xFFD3D3D3),
               size: 50.0,
+            ),
+            SizedBox(height: 20), 
+            Text(
+              'Loading...',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.white
+              ),
             ),
           ],
         ),
@@ -29,6 +77,7 @@ class LoadingScreen extends StatelessWidget {
     );
   }
 }
+
 
 class LoginPage extends StatelessWidget {
   @override
