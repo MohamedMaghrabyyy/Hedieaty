c96a747b4aaa7743c9f7808c67fff923320e026f
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2024-12-20 02:56:10 +0200

    final commit (probably)

--- hedieaty/run_integration_test.bat
+++
@@ -1,6 +0,0 @@
-
-Start-Process -NoNewWindow -FilePath adb -ArgumentList shell screenrecord --size 1440x3120 /sdcard/test_video.mp4-PassThru
-
-flutter drive --device-id="emulator-5554" --driver=./test_driver/integration_test_driver.dart --target=./test/integration_tests/full_test.dart
-
-adb pull /sdcard/test_video.mp4 test_video.mp4
